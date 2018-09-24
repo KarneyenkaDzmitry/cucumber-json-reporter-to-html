821336061fcd23ef08435d8516ee9b7157d2a4df
Dzmitry_Karneyenka <33957610+KarneyenkaDzmitry@users.noreply.github.com>
2018-09-24 15:23:21 +0300

    Update path to custom css file

--- utils/builder.js
+++ utils/builder.js
@@ -10,7 +10,7 @@ function getReportHeader(reportJson, pathToCss, title, description) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="${pathToCss}" type="text/css"/>
+    <link rel="stylesheet" href=".\node_modules\cucumber-json-reporter-to-html\css\custom.css" type="text/css"/>
     <meta charset="UTF-8">
 </head>
 <body>
