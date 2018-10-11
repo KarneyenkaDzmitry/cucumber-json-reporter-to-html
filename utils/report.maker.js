9644b48238863cae3d51f4a930934c95757f298a
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-10-11 16:52:10 +0300

    fix path

--- utils/report.maker.js
+++ utils/report.maker.js
@@ -7,7 +7,9 @@ const {getReport} = require('./report.generator.js');
 
 async function createReport(pathToReport, reportStoreHtml, title, description) {
     const reportJson = JSON.parse(fs.readFileSync(pathToReport));
-    const pathToCss = path.resolve(path.resolve(path.dirname(reportStoreHtml)), '../css/custom.css');
+    const pathToCss = path.resolve(path.resolve(path.dirname(reportStoreHtml)), path.resolve('./css/custom.css'));
+    console.log(pathToCss);
+    console.log(path.resolve('./css/custom.css'));
     const html =await getReport(reportJson, pathToCss, title, description, reportStoreHtml);
     fs.writeFileSync(reportStoreHtml, html.toString(), 'utf8');
 }
